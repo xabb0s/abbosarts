de6a04d3d43415ea3af3808abe62e757e8109e09
xabb0s <abbosxudoyberganov2005@gmail.com>
2022-05-03 13:20:48 +0500

    added theme changer to login page

---
+++ main.js
@@ -0,0 +1,35 @@
+let username = document.querySelector('.username')
+let password = document.querySelector('.password')
+let btn = document.querySelector('.btn')
+
+btn.onclick = () => {
+  if(username.value === 'xabb0s' && password.value === 'abbos2005#'){
+    window.location.href = ('./home/home.html')
+  }
+  else{
+    alert(`Login yoki parol notog'ri!`)
+  }
+}
+
+
+let color_1 = document.querySelector('.color-1')
+let color_2 = document.querySelector('.color-2')
+let color_3 = document.querySelector('.color-3')
+
+let circle = document.querySelector('.circle')
+let cube = document.querySelector('.cube')
+
+color_1.onclick = (event) => {
+  circle.style = 'background: hotpink; transition: .3s;'
+  cube.style = 'background: yellowgreen; transition: .3s;'
+} 
+
+color_2.onclick = (event) => {
+  circle.style = 'background: #68C7D9; transition: .3s;'
+  cube.style = 'background: #E50D5E; transition: .3s;'
+} 
+
+color_3.onclick = (event) => {
+  circle.style = 'background: #000000; transition: .3s;'
+  cube.style = 'background: #ffffff; transition: .3s;'
+} 
